22f7f1c5d80a125d862086a9be545e07f492c8cc
김 지안 <phexmexn@gmail.com>
2026-07-08 14:39:16 +0900

    Add files via upload
    업로드 테스트

---
+++ Exam_01.java
@@ -0,0 +1,19 @@
+
+public class Exam_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//표준(콘솔) 입출력
+		System.out.print("Hello \tWorld!");//줄바꿈 없음
+		System.out.println();
+		System.out.print("Hello \nWorld!");//줄바꿈 없음
+		System.out.print("\n");//주로 특수한 목적일 때 사
+		System.out.println("Hello World!");//자동줄바꿈
+		System.out.print("Hello World!\n");//줄바꿈
+		System.out.println("Hello World!");
+		
+		// \n ->줄바꿈, \t ->tab(우측으로 8칸 띄우기)
+		
+	}
+
+}
